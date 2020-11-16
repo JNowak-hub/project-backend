a5f74f430bc7c6c12164ac7106e4cd0f94146191
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-16 14:53:20 +0100

    Created UserService

--- src/main/java/pl/sdacademy/projectbackend/service/UserService.java
+++ src/main/java/pl/sdacademy/projectbackend/service/UserService.java
@@ -1,4 +1,17 @@
 package pl.sdacademy.projectbackend.service;
 
+import org.springframework.stereotype.Service;
+import pl.sdacademy.projectbackend.repository.UserRepository;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
 }
