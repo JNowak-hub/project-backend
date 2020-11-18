458247f90eb19c4a9286f26527dcb71076adfb64
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-18 09:05:18 +0100

    Created UserController

--- src/main/java/pl/sdacademy/projectbackend/controller/UserController.java
+++ src/main/java/pl/sdacademy/projectbackend/controller/UserController.java
@@ -1,4 +1,17 @@
 package pl.sdacademy.projectbackend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sdacademy.projectbackend.service.UserService;
+
+@RestController
+@RequestMapping("api/user")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
 }
