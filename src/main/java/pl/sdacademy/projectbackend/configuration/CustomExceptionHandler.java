7cde47f7ce2547fe1f012b538578d12fbede4f87
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-18 18:03:02 +0100

    added handling MethodArgumentNotValid for exception handling of validation

--- src/main/java/pl/sdacademy/projectbackend/configuration/CustomExceptionHandler.java
+++ src/main/java/pl/sdacademy/projectbackend/configuration/CustomExceptionHandler.java
@@ -1,16 +1,24 @@
 package pl.sdacademy.projectbackend.configuration;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.sdacademy.projectbackend.exceptions.UserNotFound;
 
 @ControllerAdvice
 @RestController
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 
     @ExceptionHandler(UserNotFound.class)
     public final ResponseEntity<Object> handleUserAlreadyExistsException(UserNotFound ex) {
