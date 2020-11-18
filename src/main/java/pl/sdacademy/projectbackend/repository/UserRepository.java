7656b18e4d1b7e0b20efa6b7816a819dad09c755
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-18 17:54:16 +0100

    Implemented basic security (Role, UserDetails, UserDetailsService, WebSecurityConfiguration) and test to loadUserByUserName (username as login)

--- src/main/java/pl/sdacademy/projectbackend/repository/UserRepository.java
+++ src/main/java/pl/sdacademy/projectbackend/repository/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sdacademy.projectbackend.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
+
+    Optional<User> findUserByLogin(String login);
 }
