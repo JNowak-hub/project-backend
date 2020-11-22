990f5c5f00f536d6fd8b59a237763e5e7abad4e3
Robert Reinert <61290384+RobertReinert@users.noreply.github.com>
2020-11-22 14:12:02 +0100

    added methods findUserByEmail and findUserByFirstNameAndLastName to UserService and UserRepository

--- src/main/java/pl/sdacademy/projectbackend/repository/UserRepository.java
+++ src/main/java/pl/sdacademy/projectbackend/repository/UserRepository.java
@@ -4,10 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sdacademy.projectbackend.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
 
     Optional<User> findUserByLogin(String login);
+    Optional<User> findUserByEmail(String email);
+    List<User> findUserByFirstNameAndLastName(String firstName, String lastName);
+
 }
