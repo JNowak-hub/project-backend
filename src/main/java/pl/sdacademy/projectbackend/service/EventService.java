563068c30258390e504f23e95ed2af3bca93318c
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-22 15:34:24 +0100

    Refactored EventService addEvent method and tested it

--- src/main/java/pl/sdacademy/projectbackend/service/EventService.java
+++ src/main/java/pl/sdacademy/projectbackend/service/EventService.java
@@ -1,29 +1,27 @@
 package pl.sdacademy.projectbackend.service;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.sdacademy.projectbackend.exceptions.EventNotFound;
 import pl.sdacademy.projectbackend.model.Event;
 import pl.sdacademy.projectbackend.model.User;
 import pl.sdacademy.projectbackend.repository.EventRepository;
+import pl.sdacademy.projectbackend.utilities.SecurityContestUtils;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class EventService {
     private EventRepository eventRepository;
+    private SecurityContestUtils securityContestUtils;
 
-    public EventService(EventRepository eventRepository) {
-
+    public EventService(EventRepository eventRepository, SecurityContestUtils securityContestUtils) {
+        this.securityContestUtils = securityContestUtils;
         this.eventRepository = eventRepository;
     }
 
     public Event addEvent(Event event) {
-        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        event.setOrganizer(loggedUser);
+        event.setOrganizer(securityContestUtils.getCurrentUser());
         return eventRepository.save(event);
     }
 
--- src/test/java/pl/sdacademy/projectbackend/service/EventServiceTest.java
+++ src/test/java/pl/sdacademy/projectbackend/service/EventServiceTest.java
@@ -6,27 +6,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.sdacademy.projectbackend.exceptions.UserNotFound;
 import pl.sdacademy.projectbackend.model.Event;
+import pl.sdacademy.projectbackend.model.Role;
 import pl.sdacademy.projectbackend.model.User;
 import pl.sdacademy.projectbackend.repository.EventRepository;
+import pl.sdacademy.projectbackend.utilities.SecurityContestUtils;
+
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EventServiceTest {
     @Mock
     private EventRepository eventRepository;
+    @Mock
+    private SecurityContestUtils securityContestUtils;
 
     @InjectMocks
     private EventService eventService;
 
     private Event testEvent;
-    private User testUser = new User();
+    private User testUser;
 
     @BeforeEach
     void setUp() {
@@ -37,6 +43,12 @@ class EventServiceTest {
         testEvent.setEndDate(LocalDateTime.of(2020, 12, 11, 21, 30));
         testEvent.setOrganizer(testUser);
         testEvent.setLocation("Poland");
+
+        testUser = new User();
+        testUser.setRole(Role.USER);
+        testUser.setLogin("login");
+        testUser.setPassword("password");
+
     }
 
     @Test
@@ -48,14 +60,29 @@ class EventServiceTest {
     }
 
     @Test
-    @DisplayName("When addEvent is called it should save Event")
+    @DisplayName("When addEvent is called it then return Event")
     void test2() {
         // given
-
+        when(securityContestUtils.getCurrentUser()).thenReturn(testUser);
+        when(eventRepository.save(any())).thenReturn(testEvent);
         // when
-
+        Event savedEvent = eventService.addEvent(testEvent);
         // then
+        assertThat(testUser).isEqualTo(savedEvent.getOrganizer());
+        assertThat(testEvent.getName()).isEqualTo(savedEvent.getName());
     }
+
+    @Test
+    @DisplayName("When addEvent is called it then return Event")
+    void test5() {
+        // given
+        when(securityContestUtils.getCurrentUser()).thenThrow(UserNotFound.class);
+        // when
+        assertThrows(UserNotFound.class, () ->  eventService.addEvent(testEvent));
+        // then
+        verify(eventRepository, never()).save(any());
+    }
+
     @Test
     @DisplayName("When findEventByIt gets not null Optional then Event should be returned")
     void test3() {
